302d143f9a908978b6a16a39f02e25174ee47277
aditya <apandey8083@gmail.com>
2016-04-13 15:43:38 +0530

    myfirstchange

---
+++ src/org/demo/practice/Main.java
@@ -0,0 +1,19 @@
+package org.demo.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreetings();
+		data.display();
+
+		}
+
+		private static void displayGreetings() {
+			System.out.println("hello gud morning");
+			System.out.println("Do Coding");
+			
+		
+	}
+
+}
